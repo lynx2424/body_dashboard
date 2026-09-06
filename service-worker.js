143391789b231008a9dc2258f7e12d8e493414b3
lynx2424 <go.west.gone.n0r1.01.22@gmail.com>
2026-09-06 14:53:50 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE='body-dashboard-v32-complete';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        return response;
+      })
+      .catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html')))
+  );
+});
